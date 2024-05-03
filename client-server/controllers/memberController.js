06518ad4b1f5c6811bb9689074c70b5c34b46c40
Yu seungbeom <dldkdlelqnzo@gmail.com>
2024-05-03 12:02:33 +0900

    fix: router, controller 리팩토링

--- client-server/controllers/memberController.js
+++ client-server/controllers/memberController.js
@@ -1,122 +1,109 @@
 import fetch from 'node-fetch';
-import fs from 'fs';
 import path, {dirname} from 'path';
 import {fileURLToPath} from "url";
 import imageUtils from "../utils/ImageUtils.js";
-import {response} from "express";
-import res from "express/lib/response.js";
 
 const __filename = fileURLToPath(import.meta.url);
 // path.resolve -> __dirname이 route의 하위 경로이므로, route의 상위 디렉토리로 이동해야 public에 접근 가능
 const __dirname = path.resolve(dirname(__filename), '..');
 const HTML_PATH = path.join(__dirname, 'public/html');
 
-const loginCheck = async (req, res) => {
-    try {
-        // JSON API 서버에 사용자가 입력한 계정 정보가 올바른지 확인해달라고 요청
-        const userInput = req.body;
+const methods = {
+    showLoginForm: (req, res) => {
+        res.sendFile(path.join(HTML_PATH, 'login.html'));
+    },
+    loginCheck: async (req, res) => {
+        try {
+            const userInput = req.body;
+            const emailRegExp = /^[a-zA-Z0-9._-]+@[a-z]+\.[a-z]{2,3}$/;
+            const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
+            if (!(emailRegExp.test(userInput.email) && passwordRegExp.test(userInput.password))) {
+                res.sendStatus(400);
+                return;
+            }
 
-        // 서버 측에서 사용자 입력이 유효한지 한 번 더 검증
-        const emailRegExp = /^[a-zA-Z0-9._-]+@[a-z]+\.[a-z]{2,3}$/;
-        const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
-        if (!(emailRegExp.test(userInput.email) && passwordRegExp.test(userInput.password))) {
-            res.sendStatus(400);
-            return;
+            const response = await fetch('http://localhost:4000/json/members/login', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userInput)
+            });
+
+            const result = await response.json();
+            if (response.ok) {
+                res.status(200).json({
+                    id: result.userId
+                });
+            } else {
+                res.sendStatus(401);
+            }
+        } catch (error) {
+            res.sendStatus(500);
+        }
+    },
+    showJoinForm: (req, res) => {
+        res.sendFile(path.join(HTML_PATH, 'join.html'));
+    },
+    join: async (req, res) => {
+        const newUser = {
+            ...req.body,
+            image: req.file.filename
         }
 
-        const response = await fetch('http://localhost:4000/json/members/login', {
-            method: 'post',
+        const response = await fetch('http://localhost:4000/json/members/join', {
+            method: 'POST',
+            body: JSON.stringify(newUser),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.status === 409) {
+            imageUtils.deleteImage('members', req.file.filename);
+        }
+
+        response.json().then(data => {
+            res.status(response.status).json(data);
+        });
+    },
+    showEditInfoForm: (req, res) => {
+        res.sendFile(path.join(HTML_PATH, 'edit-member.html'));
+    },
+    editMemberInfo: async (req, res) => {
+        const response = await fetch(`http://localhost:4000/json/members?id=${req.query.id}`, {
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(userInput)
+            body: JSON.stringify({
+                nickname: req.body.nickname,
+                image: req.file !== undefined ? req.file.filename : ""
+            })
         });
 
-        // 만약 JSON API 서버가 계정 정보가 있다고 하면, 사용자 고유번호를 클라이언트에 반환
-        const result = await response.json();
+        const json = await response.json();
         if (response.ok) {
-            res.status(200).json({
-                id: result.userId
-            });
+            res.status(200).json(json.message);
         } else {
-            res.sendStatus(401);
+            res.status(500).json(json.message);
         }
-    } catch (error) {
-        res.sendStatus(500);
-    }
-};
+    },
+    deleteMember: async (req, res) => {
+        const response = await fetch(`http://localhost:4000/json/members?id=${req.query.id}`, {
+            method: 'DELETE'
+        });
 
-const join = async (req, res) => {
-    // 사용자가 입력한 값과 이미지 파일의 이름을 결합한 새로운 사용자 객체
-    const newUser = {
-        ...req.body,
-        image: req.file.filename
-    }
-
-    const response = await fetch('http://localhost:4000/json/members/join', {
-        method: 'POST',
-        body: JSON.stringify(newUser),
-        headers: {
-            'Content-Type': 'application/json'
+        const json = await response.json();
+        if (response.ok) {
+            imageUtils.deleteImage('members', json.prevImage);
+            res.status(200).json({message: json.message});
+        } else {
+            res.status(500).json({message: json.message});
         }
-    });
-    // 만약 이메일 또는 닉네임이 중복되면, 저장해뒀던 사진을 제거
-    if (response.status === 409) {
-        imageUtils.deleteImage(req.file.filename);
+    },
+    showPasswordForm: (req, res) => {
+        res.sendFile(path.join(HTML_PATH, 'edit-password.html'));
     }
+}
 
-    response.json().then(data => {
-        res.status(response.status).json(data);
-    });
-};
-
-const showEditInfoForm = async (req, res) => {
-    res.sendFile(path.join(HTML_PATH, 'edit-member.html'));
-};
-
-const editMemberInfo = async (req, res) => {
-    const response = await fetch(`http://localhost:4000/json/members?id=${req.query.id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            nickname: req.body.nickname,
-            image: req.file !== undefined ? req.file.filename : ""
-        })
-    });
-
-    const json = await response.json();
-    if (response.ok) {
-        res.status(200).json(json.message);
-    } else {
-        res.status(500).json(json.message);
-    }
-};
-
-const deleteMember = async (req, res) => {
-    const response = await fetch(`http://localhost:4000/json/members?id=${req.query.id}`, {
-        method: 'DELETE'
-    });
-
-    const json = await response.json();
-    if (response.ok) {
-        imageUtils.deleteImage(json.prevImage);
-        res.status(200).json({message: json.message});
-    } else {
-        res.status(500).json({message: json.message});
-    }
-};
-
-const showPasswordForm = async (req, res) => {
-    res.sendFile(path.join(HTML_PATH, 'edit-password.html'));
-};
-
-export default {
-    loginCheck,
-    join,
-    showEditInfoForm,
-    editMemberInfo,
-    deleteMember,
-    showPasswordForm
-};
+export default methods;
--- client-server/routes/postRouter.js
+++ client-server/routes/postRouter.js
@@ -1,32 +1,16 @@
 import express from 'express';
-import path from 'path';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
-import multer from 'multer'; // multipart/form-data 처리용 body-parser
 
 import postController from '../controllers/postController.js';
-
-const __filename = fileURLToPath(import.meta.url);
-// path.resolve -> __dirname이 route의 하위 경로이므로, route의 상위 디렉토리로 이동해야 public에 접근 가능
-const __dirname = path.resolve(dirname(__filename), '..');
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, 'public/images/posts'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '_' + file.originalname);
-    }
-});
+import imageUtils from "../utils/ImageUtils.js";
 
 const router = express.Router();
-const upload = multer({storage: storage});
+const {uploadPosts} = imageUtils;
 
 // 게시글 등록 폼 조회
 router.get('/add-form', postController.showAddForm);
 
 // 게시글 등록
-router.post('/', upload.single('file'), postController.addPost);
+router.post('/', uploadPosts.single('file'), postController.addPost);
 
 // 게시글 조회
 router.get('/:no', postController.showPost);
@@ -35,7 +19,7 @@ router.get('/:no', postController.showPost);
 router.get('/:no/edit-form', postController.showEditForm);
 
 // 게시글 수정
-router.put('/:no', upload.single('file'), postController.editPost);
+router.put('/:no', uploadPosts.single('file'), postController.editPost);
 
 // 게시글 삭제
 router.delete('/:no', postController.deletePost);
